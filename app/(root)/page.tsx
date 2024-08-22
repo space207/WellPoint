6d88e2d56d3ed9d4b9a1ce3c46ec2324ec82afef
Davit Avetikyan <davetikyan@servicetitan.com>
2024-08-22 12:18:38 -0700

    implement initial file and folder structure

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>WellPoint</h1>
+      <p>WellPoint is a modern banking platform.</p>
+    </div>
+  );
+};
+
+export default Home;
